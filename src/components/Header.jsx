f98e2b038b86830b03901c0b3f8536ca318d78b7
mmarcelino <mariana_c_marcelino@hotmail.com>
2024-06-06 22:06:23 +0100

    changed title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import EditNoteIcon from '@mui/icons-material/EditNote';
 function Header() {
   return (
     <header>
-      {/* <h1><EditNoteIcon sx={{ fontSize: 30 }} />  Notable</h1> */}
+      {/* <h1><EditNoteIcon sx={{ fontSize: 35 }} />  Notable</h1> */}
       <h1><EditNoteIcon />  Notable</h1>
     </header>
   );
